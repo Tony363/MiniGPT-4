cbd1c8443ad131a73e911c9f2fdf1e61bec1a40d
Tony <pysolver33@gmail.com>
2025-01-22 17:45:08 -0500

    againn

--- daisee_captions/daisee_evaluation.py
+++ daisee_captions/daisee_evaluation.py
@@ -70,7 +70,8 @@ def sanitize_sentence(sentence: str) -> str:
     """
     Escapes special characters in the sentence to prevent JSON formatting issues.
     """
-    return sentence.replace('\\', '\\\\').replace('"', '\\"')
+    return sentence.replace('\n', '').replace('\\r', '').replace('\\', '\\\\')
+    # return sentence.replace('\\', '\\\\').replace('"', '\\"')
 
 def extract_json(content: str) -> str:
     """
@@ -132,30 +133,30 @@ JSON:
 Example 4:
 Sentence: "The student is Highly-Engaged."
 JSON:
-{{
+{{s
     "pred": "Highly-Engaged"
 }}
 """
-    message_content = ''
-    for att in range(retries):
+    responses = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content":prompt_template},
+            {"role": "user", "content":f"Parse the following sentence:{sentence}"}
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=128,
+        temperature=0.0,
+        n=retries
+    )      
+    message_content = ' '
+    for res in range(retries):
         try:
-            response = client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[
-                    {"role": "system", "content":prompt_template},
-                    {"role": "user", "content":f"Parse the following sentence:{sentence}"}
-                ],
-                response_format={"type": "json_object"},
-                max_tokens=128,
-                temperature=0.0,
-                n=1
-            )        
-            message_content = response.choices[0].message.content.strip()
+            message_content = sanitize_sentence(responses.choices[res].message.content.strip())
             message_content = extract_json(message_content)
             message_json = json.loads(message_content)
 
             if 'pred' not in message_json:
-                message_json['pred'] = ''
+                message_json['pred'] = message_content
                 return message_json
             return message_json
             
@@ -204,42 +205,59 @@ def get_acc(
         'The student is engaged':2,
         'The student is highly-engaged':3
     }
-    metrics = load_metrics(classes)
+    metrics_keyword = load_metrics(classes)
+    metrics_struc = load_metrics(classes)
 
     inference_samples = len(results)
     logger.info(f"INFERENCE SAMPLES - {inference_samples}")
-    pred_table,target_table = torch.zeros(inference_samples),torch.zeros(inference_samples)
-    count, json_results = 0,{}
+
+    pred_keyword,target_keyword = torch.zeros(inference_samples),torch.zeros(inference_samples)
+    pred_struc,target_struc = torch.zeros(inference_samples),torch.zeros(inference_samples)
+
+    json_results = {}
     id_key = 'video_name' if video else 'video_id'
+
     for i,sample in enumerate(results):
         answer,pred = sample['A'],sample['pred']
         pred = pred[0] if video else pred
-        count += 1
-        target_table[i] = mapping[answer]
-        pred_table[i] = target_table[i]
+        target_keyword[i] = mapping[answer]
+        pred_keyword[i] = target_keyword[i]
 
         if not check_string_in_output(pred,answer.split(' ')[-1]):
-            pred_table[i] = (target_table[i] - 1) % classes   
+            pred_keyword[i] = (target_keyword[i] - 1) % classes   
             logger.info(f"WRONG pred {sample[id_key]}")
+        metrics_keyword.forward(pred_keyword[:i + 1],target_keyword[:i + 1])
 
         parsed_json = openai_parser(pred)
         logger.info("Parsed JSON:")
         logger.info(json.dumps(parsed_json, indent=4))
         logger.info(f'\nA:{answer.split(" ")[-1]}\nP:{parsed_json["pred"]}')
         
-        if not isinstance(parsed_json['pred'],str) or\
-             parsed_json['pred'].lower() not in answer.split(' ')[-1].lower() :
-            count -= 1
-        performance = metrics.forward(pred_table[:i + 1],target_table[:i + 1])
-        json_results[sample[id_key]] = (parsed_json['pred'],answer.split(' ')[-1].lower())
+        target_struc[i] = mapping[answer]
+        pred_struc[i] = target_struc[i]
+        
+        if isinstance(parsed_json['pred'],list) or\
+            isinstance(parsed_json['pred'],dict) or\
+             parsed_json['pred'].lower() not in answer.split(' ')[-1].lower():
+            pred_struc[i] = (target_struc[i] - 1) % classes
+        metrics_struc.forward(pred_struc[:i + 1],target_struc[:i + 1])   
 
-    performance = metrics.compute()
+        json_results[sample[id_key]] = (parsed_json['pred'],answer.split(' ')[-1])
+        
+
+    performance = metrics_keyword.compute()
     logger.info(f"Key word ACC - {performance['MulticlassAccuracy']}")
     logger.info(f"Key word PR - {performance['MulticlassPrecision']}")
     logger.info(f"Key word RE - {performance['MulticlassRecall']}")
     logger.info(f"Key word F1 - {performance['MulticlassF1Score']}")
-    logger.info(f"Parsed ACC - {count/inference_samples}")
-    metrics.reset()   
+    metrics_keyword.reset()   
+
+    performance = metrics_struc.compute()
+    logger.info(f"Structure ACC - {performance['MulticlassAccuracy']}")
+    logger.info(f"Structure PR - {performance['MulticlassPrecision']}")
+    logger.info(f"Structure RE - {performance['MulticlassRecall']}")
+    logger.info(f"Structure F1 - {performance['MulticlassF1Score']}")
+    metrics_struc.reset()   
     return json_results
 
 
@@ -269,11 +287,12 @@ def main()->None:
     json_result = get_acc(results,video='video' in args.result_path)
 
     # /home/tony/nvme2tb/ieee_fer_dpo/minigpt4_eval_outputs/daisee_inference.json
-    outpath = args.resut_path.split('/')
-    if not os.path.exists(os.path.join(outpath[:-2],'structured')):
-        os.mkdir(os.path.join(outpath[:-2],'structured'))
-    with open(os.path.join(outpath[:-2],'structured',outpath[-1]),'w') as f:
-        json.dump(json_results,f,indent=4)
+    outpath = args.result_path.split('/')
+    outpath[0] = '/'
+    if not os.path.exists('structured'):
+        os.mkdir('structured')
+    with open(os.path.join('structured',outpath[-1]),'w') as f:
+        json.dump(json_result,f,indent=4)
 
 
 if __name__ == "__main__":
